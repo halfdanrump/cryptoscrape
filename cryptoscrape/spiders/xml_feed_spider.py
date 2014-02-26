a9a6a581f0f6d609fff0087301029cc1ddd1966d
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-02-26 17:37:39 +0900

    Cleans up file

--- cryptoscrape/spiders/xml_feed_spider.py
+++ cryptoscrape/spiders/xml_feed_spider.py
@@ -6,7 +6,8 @@ from cryptoscrape.items import CryptoNewsItem
 class XMLSpider(XMLFeedSpider):
     name = 'xmlfeedparser'
     #allowed_domains = ['http://techcrunch.com/']
-    start_urls = ['http://techcrunch.com/feed/']
+    #start_urls = ['http://techcrunch.com/feed/', 'feeds.gawker.com/gizmodo/full']
+    start_urls = ['http://feeds.gawker.com/gizmodo/full']
     # allowed_domains = ['rss.rssad.jp']
     # start_urls = ['rss.rssad.jp/rss/itmtop/1.0/topstory.xml']
     iterator = 'iternodes' # This is actually unnecessary, since it's the default value
@@ -17,5 +18,4 @@ class XMLSpider(XMLFeedSpider):
         item['title'] = node.xpath('title/text()').extract()
         item['link'] = node.xpath('link/text()').extract()
         item['description'] = node.xpath('description/text()').extract()
-        #print item
         return item
