625d1e4ab16e2b242dc2216e06c4cec61097abd9
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-02-26 17:34:23 +0900

    Fixed bug which made the spider crawl the same sites several times

--- cryptoscrape/spiders/rss_feed_spider.py
+++ cryptoscrape/spiders/rss_feed_spider.py
@@ -1,7 +1,28 @@
 from scrapy.spider import Spider
+from scrapy import log
+from cryptoscrape.items import CryptoNewsItem
 import feedparser
+from settings import feed_urls
 
 class RSSSpider(Spider):
 
-	def parse(self):
-		feed = 2
+	name = 'rssfeedparser'
+	#start_urls = ['http://rss.rssad.jp/rss/itmtop/1.0/topstory.xml']
+	start_urls = feed_urls
+	def parse(self, response):
+		feed_items = list()
+		log.msg('Scraping URL %s'%response.url)
+		feed = feedparser.parse(response.url)
+		
+		try:
+			for item in feed['entries']:
+				news_item = CryptoNewsItem()
+				news_item['title'] = item.get('title', None)
+				news_item['link'] = item.get('link', None)
+				news_item['description'] = item.get('description', None)
+				news_item['summary'] = item.get('summary', None)
+				feed_items.append(news_item)
+		except KeyError:
+			log.msg('Feed contained no items', level=log.WARNING)
+
+		return feed_items
